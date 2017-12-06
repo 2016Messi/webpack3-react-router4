17dadab25a6a68e3b05793f34d3d6ca882d008c1
2016Messu <wy99599@163.com>
2017-12-06 14:18:21 +0800

    PC端 移动端 页头页脚

--- src/js/components/PC_index.js
+++ src/js/components/PC_index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PCHeader from './PC_header';
+import PCFooter from './PC_footer';
 export default class PCIndex extends React.Component {
     render() {
         return (
             <div>
                 <PCHeader></PCHeader>
+                <PCFooter></PCFooter>
             </div>
         );
     };
