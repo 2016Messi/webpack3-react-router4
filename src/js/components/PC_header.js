8ebeb1b0323797228748aac92148af1302087517
2016Messu <wy99599@163.com>
2017-12-07 22:10:19 +0800

    07/12 最后一次修改 ，已完成注册功能模块

--- src/js/components/PC_header.js
+++ src/js/components/PC_header.js
@@ -3,7 +3,8 @@ import { Row, Col } from 'antd';
 import { Menu, Icon, Tabs, message, Form, Input, Button, Checkbox, Modal} from 'antd';
 
 const FormItem = Form.Item;
-export default class PCHeader extends React.Component {
+const TabPane = Tabs.TabPane;
+  class PCHeader extends React.Component {
     constructor(){
         super();
         this.state = {
@@ -15,13 +16,42 @@ export default class PCHeader extends React.Component {
             userId: 0
         }
     }
+    setModalVisible(value){
+        this.setState({modalVisible:value});
+    };
+    handleSubmit(e){
+
+        //页面开始想 API 进行数据交换
+        e.preventDefault();
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        console.log(formData);
+
+         fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password" +
+             "&r_userName=" + formData.r_userName +
+             "&r_password=" + formData.r_password +
+             "&r_confirmPassword=" + formData.r_confirmPassword,
+             myFetchOptions).
+        then(response =>  response.json()).
+         then(json=>this.setState({userNickName:json.NickUserName,userid:json.UserId}));
+        message.success("请求成功！");
+        this.setModalVisible(false);
+    }
     handleClick (e){
-        this.setState({
-            current:e.key
-        })
+        if(e.key=="register"){
+            this.setState({current:"register"});
+            this.setModalVisible(true);
+        }else{
+            this.setState({
+                current:e.key
+            })
+        }
+
     };
     render(){
-        // let {getFieldProps} = this.props.from;
+        let {getFieldProps} = this.props.form;
         const userShow = this.state.hasLogined ?
                 <Menu.Item key="logout" class="register">
                     <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
@@ -74,8 +104,23 @@ export default class PCHeader extends React.Component {
                             </Menu.Item>
                             {userShow}
                             <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onOk={()=>this.setModalVisible(false)} onText="关闭" onCancel={()=>this.setModalVisible(false)}>
-                                <Tabs>
+                                <Tabs type="card">
+                                    <TabPane tab={"注册"} key={"2"}>
+                                        <Form layout={"horizontal"}  onSubmit={this.handleSubmit.bind(this)}>
+                                            <FormItem label="账户" >
+                                                <Input type="text" placeholder="请输入您的账号" {...getFieldProps("r_userName")} />
+                                            </FormItem>
+                                            <FormItem label={"密码"} >
+                                                <Input type="password" placeholder="请输入您的密码" {...getFieldProps("r_password")} />
+                                            </FormItem>
+                                            <FormItem label={"确认密码"} >
+                                                <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps("r_confirmPassword")} />
+                                            </FormItem>
+                                            <Button type="primary" htmlType="submit" >注册</Button>
 
+                                        </Form>
+
+                                    </TabPane>
                                 </Tabs>
                             </Modal>
                         </Menu>
@@ -88,4 +133,4 @@ export default class PCHeader extends React.Component {
     }
 }
 
-// export default PCHeader = Form.create({})(PCHeader);
+export default PCHeader = Form.create({})(PCHeader);
--- src/js/components/mobile_header.js
+++ src/js/components/mobile_header.js
@@ -1,14 +1,82 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import { Menu, Icon, Tabs, message, Form, Input, Button, Checkbox, Modal} from 'antd';
 
-export default class MobileHeader extends React.Component {
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
+ class MobileHeader extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            current:"top",
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: "",
+            userId: 0
+        }
+    }
+    setModalVisible(value){
+        this.setState({modalVisible:value});
+    };
+    handleSubmit(e){
+
+        //页面开始想 API 进行数据交换
+        e.preventDefault();
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        console.log(formData);
+
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password" +
+            "&r_userName=" + formData.r_userName +
+            "&r_password=" + formData.r_password +
+            "&r_confirmPassword=" + formData.r_confirmPassword,
+            myFetchOptions).
+        then(response =>  response.json()).
+        then(json=>this.setState({userNickName:json.NickUserName,userid:json.UserId}));
+        message.success("请求成功！");
+        this.setModalVisible(false);
+    }
+     login(){
+         this.setModalVisible(true);
+     };
     render(){
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogined ?
+            <Link>
+                <Icon type="inbox"/>
+            </Link>
+            :
+            <Icon type="setting" onClick={this.login.bind(this)}/>;
         return (
             <div id="mobileheader">
                 <header>
-                    <img src="./src/images/logo.png" alt="logo"/>
+                    <img src="./src/image/logo.png" alt="logo"/>
                     <span>Hot News</span>
+                    {userShow}
                 </header>
+                <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText = "关闭">
+                    <Tabs type="card">
+                        <TabPane tab="注册" key="2">
+                            <Form  onSubmit={this.handleSubmit.bind(this)}>
+                                <FormItem label="账户">
+                                    <Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
+                                </FormItem>
+                                <FormItem label="密码">
+                                    <Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
+                                </FormItem>
+                                <FormItem label="确认密码">
+                                    <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
+                                </FormItem>
+                                <Button type="primary" htmlType="submit" >注册</Button>
+                            </Form>
+                        </TabPane>
+                    </Tabs>
+                </Modal>
             </div>
         )
     }
 }
+export default MobileHeader = Form.create({})(MobileHeader);
