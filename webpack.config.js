cf3d2afcdb9c33d101008dcbc349b3022d29acf9
2016Messu <wy99599@163.com>
2017-12-22 09:15:40 +0800

    12/21  完成所有功能模块的开发，已优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: __dirname + "/src/js/root.js",               //已多次提及的唯一入口文件
     output: {
         path: path.resolve(__dirname, "src/js"),     //打包后的文件存放的地方
         filename: "bundle.js"//打包后输出文件的文件名
     },
+    // devtool: 'false',
     devtool: 'eval-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, "./"),//本地服务器所加载的页面所在的目录
